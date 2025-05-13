cca9d1a40899e72764fcf96d5cc7a30ddc242b67
Satyam Singh <38757842+SatyamSingh8449@users.noreply.github.com>
2025-05-14 00:59:00 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   base: '/Web/', // 👈 Update with your actual repo name
   build: {
     assetsPrefix: '/Web/', // 👈 Must match `base`
-    outDir: 'dist', // 👈 required for peaceiris GH pages action
+    outDir: './dist', // 👈 required for peaceiris GH pages action
   },
   output: 'static', // 👈 required for GH Pages
   integrations: [tailwind()],
