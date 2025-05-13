63e2659e07bc0dfa6e6e9d868c11ec1e6fb34f4d
Mukulrajput911 <44765703+Mukulrajput911@users.noreply.github.com>
2025-05-14 01:06:18 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mukulrajput911.github.io' , 
   base: '/Web/', // 👈 Update with your actual repo name
   
   output: 'static', // 👈 required for GH Pages
