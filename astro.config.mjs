95762691d304b7c0a24006db6f22ef1b6398a18e
Mukulrajput911 <44765703+Mukulrajput911@users.noreply.github.com>
2025-05-14 01:04:56 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   base: '/Web/', // 👈 Update with your actual repo name
-  build: {
-    assetsPrefix: '/Web/', // 👈 Must match `base`
-    outDir: 'dist', // 👈 required for peaceiris GH pages action
-  },
+  
   output: 'static', // 👈 required for GH Pages
   integrations: [tailwind()],
 });
